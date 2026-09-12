ee932c7c546eab02b4cef23ac92208bb3fb00eba
eumariegailbaydal <eumariegailbaydal@gmail.com>
2026-09-13 00:36:22 +0800

    Update middleware.ts

--- ship-management/middleware.ts
+++ ship-management/middleware.ts
@@ -26,15 +26,15 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  const isLoginPage = request.nextUrl.pathname.startsWith("/login");
+  const isPublicPage = request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/apply");
 
-  if (!user && !isLoginPage) {
+  if (!user && !isPublicPage) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
-  if (user && isLoginPage) {
+  if (user && request.nextUrl.pathname.startsWith("/login")) {
     const url = request.nextUrl.clone();
     url.pathname = "/dashboard";
     return NextResponse.redirect(url);
